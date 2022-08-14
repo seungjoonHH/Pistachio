e7d3bafec50c75e5b1169fc5e74396c295786794
sam2244 <phenix2244@naver.com>
2022-08-14 23:07:11 +0900

    notification comment update

--- lib/presenter/notification.dart
+++ lib/presenter/notification.dart
@@ -9,9 +9,7 @@ class NotificationPresenter extends GetxController {
 
   @override
   void onInit() async{
-    /// 첫 빌드시, 권한 확인하기
-    /// ios는 필수, 안드로이드는 따로 유저가 설정하지 않는 한 상관 없음.
-    /// 자동 권한 확보 상태
+    // 첫 빌드시, 권한 확인
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
       announcement: false,
@@ -27,53 +25,46 @@ class NotificationPresenter extends GetxController {
     _onMessage();
     super.onInit();
   }
-  /// 디바이스 고유 토큰을 얻기 위한 메소드, 처음 한번만 사용해서 토큰을 확보하자.
-  /// 이는 파이어베이스 콘솔에서 손쉽게 디바이스에 테스팅을 할 때 쓰인다.
+  // 디바이스 고유 토큰 가져오기
   void _getToken() async{
     String? token= await messaging.getToken();
     try{
       print(token);
     } catch(e) {}
   }
-  /// ----------------------------------------------------------------------------
 
   /// * 안드로이드에서 foreground 알림 위한 flutter_local_notification 라이브러리 *
-  /// 1. channel 생성 (우리의 알림을 따로 전달해줄 채널을 직접 만든다)
-  /// 2. 그 채널을 우리 메인 채널로 정해줄 플러그인을 만들어준다.
+  // channel 생성 (알림을 따로 전달해줄 채널을 직접 생성)
   final AndroidNotificationChannel channel = const AndroidNotificationChannel(
     'high_importance_channel', // id
     'High Importance Notifications', // title
     description: 'This channel is used for important notifications.', // description
     importance: Importance.max,
   );
-  // 2.
+  // 생성한 채널을 우리 메인 채널로 정해줄 플러그인을 만들어준다.
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
   FlutterLocalNotificationsPlugin();
 
   void _onMessage() async{
     /// * local_notification 관련한 플러그인 활용 *
-    /// 1. 위에서 생성한 channel 을 플러그인 통해 메인 채널로 설정한다.
-    /// 2. 플러그인을 초기화하여 추가 설정을 해준다.
-
-    // 1.
+    // 위에서 생성한 channel 을 플러그인 통해 메인 채널로 설정한다.
     await flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
         ?.createNotificationChannel(channel);
-    // 2.
+    // 플러그인을 초기화하여 추가 설정을 해준다.
     await flutterLocalNotificationsPlugin.initialize(
         const InitializationSettings(
             android: AndroidInitializationSettings('@mipmap/ic_launcher'), iOS: IOSInitializationSettings()),
         onSelectNotification: (String? payload) async {});
 
-    /// * onMessage 설정 - 이것만 설정해줘도 알림을 받아낼 수 있다. *
-
+    /// * onMessage 설정 *
     // 1. 콘솔에서 발송하는 메시지를 message 파라미터로 받아온다.
-    /// 메시지가 올 때마다 listen 내부 콜백이 실행된다.
+    // 메시지가 올 때마다 listen 내부 콜백이 실행
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       RemoteNotification? notification = message.notification;
       AndroidNotification? android = message.notification?.android;
 
-      // android 일 때만 flutterLocalNotification 을 대신 보여주는 거임. 그래서 아래와 같은 조건문 설정.
+      // android 일 때만 flutterLocalNotification 을 대신 보여준다
       if (notification != null && android != null) {
         flutterLocalNotificationsPlugin.show(
           notification.hashCode,
@@ -87,7 +78,7 @@ class NotificationPresenter extends GetxController {
             ),
           ),
 
-          // 넘겨줄 데이터가 있으면 아래 코드를 써주면 됨.
+          // 넘겨줄 데이터가 있으면 아래 코드 사용.
           // payload: message.data['argument']
         );
       }

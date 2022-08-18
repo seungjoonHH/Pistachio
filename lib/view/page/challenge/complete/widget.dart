53bc5b9e60a55058ab8d70a14adc4e12d59a6ed3
sam2244 <phenix2244@naver.com>
2022-08-18 17:16:05 +0900

    challenge complete update

--- lib/view/page/challenge/complete/widget.dart
+++ lib/view/page/challenge/complete/widget.dart
@@ -1,4 +1,4 @@
-/* 챌린지 디테일 위젯 */
+/* 챌린지 완료 위젯 */
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -6,11 +6,10 @@ import 'package:get/get.dart';
 import 'package:pistachio/global/theme.dart';
 import 'package:pistachio/model/class/challenge.dart';
 import 'package:pistachio/presenter/model/challenge.dart';
-import 'package:pistachio/presenter/page/challenge/complete.dart';
 import 'package:pistachio/view/widget/button/button.dart';
 import 'package:pistachio/view/widget/widget/text.dart';
 
-// 챌린지 디테일 리스트 뷰
+// 챌린지 완료 리스트 뷰
 class ChallengeListView extends StatelessWidget {
   const ChallengeListView({Key? key}) : super(key: key);
 
@@ -30,8 +29,8 @@ class ChallengeListView extends StatelessWidget {
   }
 }
 
-class ChallengeDetailAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const ChallengeDetailAppBar({Key? key}) : super(key: key);
+class ChallengeCompleteAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const ChallengeCompleteAppBar({Key? key}) : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(60.0);
@@ -64,27 +63,27 @@ class ChallengeDetailBody extends StatelessWidget {
       children: [
         Center(
           child: SvgPicture.asset(
-            challenge.imageUrls['default'],
-            width: 500.0,
+            challenge.imageUrls['complete'],
+            width: 360.0,
             fit: BoxFit.fitHeight,
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(34.0),
           child: PText(
-            challenge.descriptions['detail']!.replaceAll('#', ''),
+            challenge.descriptions['complete']!.replaceAll('#', ''),
             style: textTheme.titleSmall,
             color: PTheme.white,
             maxLines: 7,
           ),
         ),
         Container(
-          height: 80.0,
+          height: 130.0,
         ),
         Center(
           child: PButton(
-            onPressed: () => ChallengeComplete.toChallengeComplete(challenge),
-            text: '챌린지 하러가기',
+            onPressed: () => ChallengePresenter.toChallengeMain(),
+            text: '컬랙션 보러가기',
             //backgroundColor: challenge.theme['button'],
           ),
         )

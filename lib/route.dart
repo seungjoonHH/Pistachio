506eb4e91326b83a4fc6bdfdbc3a252bfa876365
Bokwon Choi <89948250+Vincenchoi@users.noreply.github.com>
2023-01-12 10:12:17 +0900

    Map Input Update

--- lib/route.dart
+++ lib/route.dart
@@ -41,7 +41,7 @@ class PRoute {
         '/login': const LoginPage(),
         '/register': const RegisterPage(),
         '/onboarding': const OnboardingPage(),
-        '/exercise/input': const ExerciseInputPage(),
+        '/exercise/input': ExerciseInputPage(),
         '/record/main': const RecordMainPage(),
         '/record/detail': const RecordDetailPage(),
         '/challenge/main': const ChallengeMainPage(),
--- lib/view/page/exercise/input/input.dart
+++ lib/view/page/exercise/input/input.dart
@@ -1,24 +1,94 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geolocator/geolocator.dart' as geo;
+import 'package:get/get.dart' as gett;
+import 'package:native_exif/native_exif.dart';
 import 'package:pistachio/global/theme.dart';
 import 'package:pistachio/model/enum/activity_type.dart';
 import 'package:pistachio/presenter/page/exercise/input.dart';
 import 'package:pistachio/view/page/exercise/input/widget.dart';
 import 'package:pistachio/view/widget/button/button.dart';
 import 'package:pistachio/view/widget/widget/app_bar.dart';
+import 'package:pistachio/view/widget/widget/card.dart';
 import 'package:pistachio/view/widget/widget/text.dart';
+import 'package:super_banners/super_banners.dart';
+import 'package:dio/dio.dart';
 
-class ExerciseInputPage extends StatelessWidget {
-  const ExerciseInputPage({Key? key}) : super(key: key);
+class ExerciseInputPage extends StatefulWidget {
+  const ExerciseInputPage({super.key});
+
+  @override
+  State<ExerciseInputPage> createState() => _ExerciseInputPageState();
+}
+
+class _ExerciseInputPageState extends State<ExerciseInputPage> {
+
+  bool textScanning = false;
+  XFile? imageFile;
+  String scannedText = '';
+  Exif? exif;
+  ExifLatLong? coordinates;
+  double? distance;
+  String myLocation = '';
+  String address = "";
+
+
+  String googleApikey = "AIzaSyBrAdaZUxs-rN6KR2ExrqpKQHnZBRH0uQ4";
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  //유저의 현재 위치
+  Future<geo.Position> getUserCurrentLocation() async {
+    await geo.Geolocator.requestPermission().then((value){
+    }).onError((error, stackTrace) async {
+      await geo.Geolocator.requestPermission();
+      print("ERROR"+error.toString());
+    });
+    return await geo.Geolocator.getCurrentPosition();
+  }
+
+  //위도와 경도를 영문주소로 변환
+  void convertToAddress(double lat, double long, String apikey) async {
+    Dio dio = Dio();  //initilize dio package
+    String apiurl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=$apikey";
+
+    Response response = await dio.get(apiurl) as Response; //send get request to API URL
+
+    if(response.statusCode == 200){ //if connection is successful
+      Map data = response.data; //get response data
+      if(data["status"] == "OK"){ //if status is "OK" returned from REST API
+        if(data["results"].length > 0){ //if there is atleast one address
+          Map firstresult = data["results"][0]; //select the first address
+
+          address = firstresult["formatted_address"]; //get the address
+
+          //you can use the JSON data to get address in your own format
+
+          setState(() {
+            //refresh UI
+          });
+        }
+      }else{
+        print(data["error_message"]);
+      }
+    }else{
+      print("error while fetching geoconding data");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (Get.arguments == null) return const Scaffold();
-    ActivityType type = Get.arguments;
+    if (gett.Get.arguments == null) return const Scaffold();
+    ActivityType type = gett.Get.arguments;
 
     Map<ActivityType, List<String>> messages = {
       ActivityType.calorie: ['오늘 ', '참은 음식', '을 선택해주세요.'],
-      ActivityType.distance: ['오늘 한 ', '유산소 운동 시간', '을 입력해주세요.'],
+      ActivityType.distance: ['어디서', ' 출발', '했나요'],
+      // ActivityType.distance: ['오늘', ' 달린 거리', '를 입력해주세요.'],
       ActivityType.height: ['오늘 ', '오른 층 수', '를 입력해주세요.'],
       ActivityType.weight: ['오늘 ', '운동한 횟수', '를 입력해주세요.'],
     };
@@ -46,7 +116,7 @@ class ExerciseInputPage extends StatelessWidget {
     };
 
     Map<ActivityType, String> riveAnimations = {
-      ActivityType.calorie: 'Example',
+      ActivityType.calorie: '',
       ActivityType.distance: 'Jog',
       ActivityType.height: 'Animation 1',
       ActivityType.weight: 'Demo',
@@ -65,45 +135,132 @@ class ExerciseInputPage extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  PTexts(messages[type]!,
+                  PTexts(
+                    messages[type]!,
                     alignment: MainAxisAlignment.start,
                     colors: [PTheme.black, type.color, PTheme.black],
-                    style: textTheme.bodyLarge,
+                    style: textTheme.displaySmall,
                     space: false,
                   ),
-                  PText('피스타치오가 알아서 계산해 줄게요.',
-                    style: textTheme.bodyLarge,
+                  PText(
+                    '하쿠나님?',
+                    style: textTheme.displaySmall,
                   ),
                   const SizedBox(height: 20.0),
-                  GetBuilder<ExerciseInput>(
-                    builder: (controller) {
-                      return PInputField(
-                        controller: controller.inputCont,
-                        hintText: controller.hintText ?? hints[type]!,
-                        keyboardType: TextInputType.number,
-                        invalid: controller.invalid,
-                        hintColor: controller.hintText == null
-                            ? PTheme.grey : PTheme.colorB,
-                      );
-                    }
-                  ),
+                  // GetBuilder<ExerciseInput>(
+                  //   builder: (controller) {
+                  //     return PInputField(
+                  //       controller: controller.inputCont,
+                  //       hintText: controller.hintText ?? hints[type]!,
+                  //       keyboardType: TextInputType.number,
+                  //       invalid: controller.invalid,
+                  //       hintColor: controller.hintText == null
+                  //           ? PTheme.grey : PTheme.colorB,
+                  //     );
+                  //   }
+                  // ),
+                  PCard(
+                    onPressed: () {
+                      getUserCurrentLocation().then((value) async {
+                        convertToAddress(value.latitude, value.longitude, googleApikey);
+                      });
+
+                    },
+                      rounded: true,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          PText(
+                            '현재 내 위치 기반으로 할래요!',
+                            color: PTheme.colorB,
+                            style: textTheme.titleMedium,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(Icons.my_location, color: PTheme.colorA,),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              PText('내 위치:', color: PTheme.colorA, style: textTheme.titleMedium,),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                width: 180,
+                                  child: PText(address, color: PTheme.black, style: textTheme.titleSmall, maxLines: 3,))
+                            ],
+                          )
+                        ],
+                      )),
+                  Row(
+                    children: [
+                      PCard(
+                          rounded: true,
+                          child: Column(
+                            children: [
+                              PText(
+                                '내 위치\n사진 업로드',
+                                maxLines: 2,
+                                style: textTheme.titleLarge,
+                                color: PTheme.colorC,
+                              ),
+                              Image.asset('assets/image/page/input/streetView.png', width: 100,)
+                            ],
+                          )),
+                      PCard(
+                        padding: EdgeInsets.zero,
+                          rounded: true,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                top:0,
+                                right: 0,
+                                child: CornerBanner(
+                                    bannerColor: PTheme.colorB,
+                                    bannerPosition: CornerBannerPosition.topRight,
+                                    child: PText('Beta')),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(20.0.r),
+                                child: Column(
+                                  children: [
+                                    PText(
+                                      '내 주소\n인식하기',
+                                      maxLines: 2,
+                                      style: textTheme.titleLarge,
+                                      color: PTheme.colorC,
+                                    ),
+                                    Image.asset('assets/image/page/input/destinationView.png', width: 100,)
+                                  ],
+                                ),
+                              )
+
+                            ],
+                          ))
+                    ],
+                  )
                 ],
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: RAnimation(
-                    rName: riveAssets[type]!,
-                    abName: riveArtboard[type]!,
-                    aName: riveAnimations[type]!,
-                  ),
-                ),
-              ),
-              GetBuilder<ExerciseInput>(
+              // Expanded(
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(20.0),
+              //     child: RAnimation(
+              //       rName: riveAssets[type]!,
+              //       abName: riveArtboard[type]!,
+              //       aName: riveAnimations[type]!,
+              //     ),
+              //   ),
+              // ),
+              gett.GetBuilder<ExerciseInput>(
                 builder: (controller) {
                   return PButton(
                     onPressed: () => controller.completeButtonPressed(type),
-                    text: '입력 완료',
+                    text: '선택 완료',
                     stretch: true,
                     backgroundColor: type.color,
                     textColor: PTheme.black,
@@ -117,3 +274,4 @@ class ExerciseInputPage extends StatelessWidget {
     );
   }
 }
+
